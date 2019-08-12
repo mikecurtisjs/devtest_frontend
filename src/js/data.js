a5d2dc70905268a7730b6badf1bd48f90196a891
mikecurtisjs <32725661+mikecurtisjs@users.noreply.github.com>
2019-08-12 08:03:23 -0700

    Data is a const

--- src/js/data.js
+++ src/js/data.js
@@ -1,11 +1,14 @@
-let data = [];
+// Main State
+const data = [];
+
+// Filter Data
 let filteredData = [];
 
 (function() {
   fetch("https://jsonplaceholder.typicode.com/todos")
     .then(res => res.json())
     .then(json => {
-      data = json;
+      data.push(...json);
       displayData(data);
       displayFilter(data.map(task => task.userId));
     })
